6f51ab6c473509cb58c1e0133532345ce0805663
Aurorabili <aurorabili@outlook.com>
2023-12-29 15:31:22 +0000

    Refactor tree traversal algorithm to use smart pointers
    Signed-off-by: Aurorabili <aurorabili@outlook.com>

--- template/algorithms/tree/wpl.cpp
+++ template/algorithms/tree/wpl.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
+#include <memory>
 
 struct node;
-using node_ptr = node*;
+using node_ptr = std::unique_ptr<node>;
 struct node {
   int weight;
   node_ptr left;
   node_ptr right;
 };
 
-int dfs(node_ptr node, int depth) {
+int dfs(const node* node, int depth) {
   if (!node->left && !node->right) return node->weight * depth;
 
   int sum = 0;
-  if (node->left) sum += dfs(node->left, depth + 1);
-  if (node->right) sum += dfs(node->right, depth + 1);
+  if (node->left) sum += dfs(node->left.get(), depth + 1);
+  if (node->right) sum += dfs(node->right.get(), depth + 1);
   return sum;
 }
 
@@ -26,26 +27,30 @@ int main() {
     std::cin >> weight[i];
   }
 
-  node_ptr root_ = new node{weight[0], nullptr, nullptr};
+  node_ptr root_ = std::make_unique<node>();
+  root_->weight = weight[0];
 
   for (int i = 0; i < n - 1; ++i) {
     int parent, child;
     std::cin >> parent >> child;
     if (parent == 0) {
-      root_->left = new node{weight[child], nullptr, nullptr};
+      root_->left = std::make_unique<node>();
+      root_->left->weight = weight[child];
     } else {
-      node_ptr parent_ = root_;
+      node* parent_ = root_.get();
       for (int j = 1; j < parent; ++j) {
-        parent_ = parent_->left;
+        parent_ = parent_->left.get();
       }
       if (!parent_->left) {
-        parent_->left = new node{weight[child], nullptr, nullptr};
+        parent_->left = std::make_unique<node>();
+        parent_->left->weight = weight[child];
       } else {
-        parent_->right = new node{weight[child], nullptr, nullptr};
+        parent_->right = std::make_unique<node>();
+        parent_->right->weight = weight[child];
       }
     }
   }
 
-  std::cout << dfs(root_, 0) << std::endl;
+  std::cout << dfs(root_.get(), 0) << std::endl;
   return 0;
 }
